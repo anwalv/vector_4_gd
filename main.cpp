3903ee998d0a137ebd432a0c08cc76c8f62ebfad
Anna Oleksandrovych <150846802+anwalv@users.noreply.github.com>
2025-04-10 16:34:29 +0300

    created dot prod func

--- main.cpp
+++ main.cpp
@@ -65,4 +65,31 @@ public:
         return *this;
     }
 
+    vector4& div(float scale) {
+        __m128 scalar = _mm_set1_ps(scale);
+        data = _mm_div_ps(data, scalar);
+        return *this;
+    }
+
+    vector4& div(float scale, float w_scale) {
+        __m128 scalar = _mm_set_ps(w_scale, scale, scale, scale);
+        data = _mm_div_ps(data, scalar);
+        return *this;
+    }
+
+    vector4& dot(const vector4 &other) {
+        __m128 mul = _mm_mul_ps(data, other.data);
+        __m128 shuf = _mm_shuffle_ps(mul, mul, _MM_SHUFFLE(2, 3, 0, 1));
+        __m128 sums = _mm_add_ps(mul, shuf);
+        shuf = _mm_movehl_ps(shuf, sums);
+        __m128 dotSingleValue = _mm_add_ss(sums, shuf);
+        data = _mm_shuffle_ps(dotSingleValue, dotSingleValue, 0);
+        return *this;
+    }
+
+    vector4& dot(float x, float y, float z) {
+        vector4 other(x, y, z);
+        return dot(other);
+    }
+
 };
