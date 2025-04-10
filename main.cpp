d041b3be9e9d3a9eb144860ef78afa1d460f5d7a
Anna Oleksandrovych <150846802+anwalv@users.noreply.github.com>
2025-04-10 10:57:07 +0300

    created struct vector 4

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <immintrin.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+
+struct vector4 {
+private:
+    __m128 data;
+
+public:
+    vector4(float x, float y, float z) {
+        data = _mm_set_ps(0.0f, z, y, x);
+    }
+
+    vector4(float x, float y, float z, float w) {
+        data = _mm_set_ps(w, z, y, x);
+    }
+
+    float x() const {
+        return _mm_cvtss_f32(data);
+    }
+
+    float y() const {
+        return _mm_cvtss_f32(_mm_shuffle_ps(data, data, _MM_SHUFFLE(1, 1, 1, 1)));
+    }
+
+    float z() const {
+        return _mm_cvtss_f32(_mm_shuffle_ps(data, data, _MM_SHUFFLE(2, 2, 2, 2)));
+    }
+
+    float w() const {
+        return _mm_cvtss_f32(_mm_shuffle_ps(data, data, _MM_SHUFFLE(3, 3, 3, 3)));
+    }
+};
