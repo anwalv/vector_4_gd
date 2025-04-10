2feb9a701dbe55cf37aa18b897852667a22342e3
Anna Oleksandrovych <150846802+anwalv@users.noreply.github.com>
2025-04-10 16:43:55 +0300

    some tests

--- main.cpp
+++ main.cpp
@@ -91,5 +91,71 @@ public:
         vector4 other(x, y, z);
         return dot(other);
     }
+    void print(const std::string& label = "") const {
+        if (!label.empty()) {
+            std::cout << label << ": ";
+        }
+        std::cout << "(" << x() << ", " << y() << ", " << z() << ", " << w() << ")" << std::endl;
+    }
 
 };
+int main() {
+    //------------------------------------------
+    std::cout << "===Test1===" << std::endl;
+    vector4 v1(1.0f, 2.0f, 3.0f);
+    vector4 v2(4.0f, 5.0f, 6.0f, 1.0f);
+
+    v1.print("v1");
+    v2.print("v2");
+
+    //------------------------------------------
+    std::cout << "\n=== Test2 ===" << std::endl;
+    vector4 v3 = v1;
+    v3.add(v2);
+    v3.print("v1 + v2");
+
+    vector4 v4 = v1;
+    v4.add(2.0f, 3.0f, 4.0f);
+    v4.print("v1 + (2,3,4)");
+
+    //------------------------------------------
+    std::cout << "\n=== Test3 ===" << std::endl;
+    vector4 v5 = v2;
+    v5.sub(v1);
+    v5.print("v2 - v1");
+
+    vector4 v6 = v2;
+    v6.sub(1.0f, 1.0f, 1.0f);
+    v6.print("v2 - (1,1,1)");
+
+    //------------------------------------------
+    std::cout << "\n===Test4===" << std::endl;
+    vector4 v7 = v1;
+    v7.mul(2.0f);
+    v7.print("v1 * 2");
+
+    vector4 v8 = v2;
+    v8.mul(3.0f, 0.5f);
+    v8.print("v2 * (3,3,3,0.5)");
+
+    //------------------------------------------
+    std::cout << "\n=== Test 5 ===" << std::endl;
+    vector4 v9 = v1;
+    v9.div(2.0f);
+    v9.print("v1 / 2");
+
+    vector4 v10 = v2;
+    v10.div(2.0f, 0.5f);
+    v10.print("v2 / (2,2,2,0.5)");
+    //------------------------------------------
+    std::cout << "\n=== Test 6 ===" << std::endl;
+    vector4 v11 = v1;
+    v11.dot(v2);
+    v11.print("v1 * v2");
+
+    vector4 v12 = v2;
+    v12.dot(1.0f, 2.0f, 3.0f);
+    v12.print("v2 * (1,2,3)");
+
+    return 0;
+}
